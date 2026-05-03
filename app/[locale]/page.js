676d8315b7440727b37a127b53d584c021449a52
keroles adel <kerolesadel297@gmail.com>
2026-05-03 23:25:58 +0300

    add commponnt in app/[locale]/page.js

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -1,11 +1,20 @@
-import Navbar from "@/components/layout/Navbar";
-import Hero from "@/components/sections/Hero";
+import Contact from "../../components/sections/Contact";
+import FeedBack from "@/components/sections/FeedBack";
+import Hero from "../../components/sections/Hero";
+import Projects from "../../components/sections/Projects";
+import Timeline from "../../components/sections/Timeline";
+import WhyChooseUs from "../../components/sections/WhyChooseUs";
 import { useTranslations } from "next-intl";
 export default function Home() {
   const t = useTranslations("nav");
   return (
     <>
       <Hero />
+      <WhyChooseUs />
+      <Projects />
+      <FeedBack />
+      <Timeline />
+      <Contact />
     </>
   );
 }
