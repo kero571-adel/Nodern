657b772a0442ddf6629a478da6ecf8b6e337c15a
Beshoy kameal sadiek <158048643+beshoo12@users.noreply.github.com>
2026-05-06 20:30:47 +0300

    Add feedback section and projects section

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -1,9 +1,9 @@
-import Contact from "../../components/sections/Contact";
+import Contact from "@/components/sections/Contact";
 import FeedBack from "@/components/sections/FeedBack";
-import Hero from "../../components/sections/Hero";
-import Projects from "../../components/sections/Projects";
-import Timeline from "../../components/sections/Timeline";
-import WhyChooseUs from "../../components/sections/WhyChooseUs";
+import Hero from "@/components/sections/Hero";
+import Projects from "@/components/sections/Projects";
+import Timeline from "@/components/sections/Timeline";
+import WhyChooseUs from "@/components/sections/WhyChooseUs";
 import { useTranslations } from "next-intl";
 export default function Home() {
   const t = useTranslations("nav");
